e4d38497488f5ff4df2c58d0be572f886b4297a7
Marcin Weklar <weklar10@gmail.com>
2021-11-30 18:53:52 +0100

    implement map based services

---
+++ pet-clinic-data/src/main/java/pl/marcin/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package pl.marcin.petclinic.services.map;
+
+import pl.marcin.petclinic.model.Pet;
+import pl.marcin.petclinic.services.CrudService;
+import pl.marcin.petclinic.services.PetService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
